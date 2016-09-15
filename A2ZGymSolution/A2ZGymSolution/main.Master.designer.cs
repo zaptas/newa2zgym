4f6272c7b84c01191a656c88a604ab03824f2e50
deep1024 <deepak.khatri@zaptas.com>
2016-09-15 16:42:51 +0530

    change

--- A2ZGymSolution/A2ZGymSolution/main.Master.designer.cs
+++ A2ZGymSolution/A2ZGymSolution/main.Master.designer.cs
@@ -139,15 +139,6 @@ namespace A2ZGymSolution {
         protected global::System.Web.UI.WebControls.Button btnNewUser;
         
         /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-        
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
