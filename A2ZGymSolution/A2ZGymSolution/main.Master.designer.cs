007fc6b1308c5429fa59b01ecf3e72761026a01a
sohel khan office <sohelkhan00000@gmail.com>
2016-09-15 17:30:09 +0530

    test

--- A2ZGymSolution/A2ZGymSolution/main.Master.designer.cs
+++ A2ZGymSolution/A2ZGymSolution/main.Master.designer.cs
@@ -31,24 +31,6 @@ namespace A2ZGymSolution {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// signinemail control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText signinemail;
-        
-        /// <summary>
-        /// signinpassword control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText signinpassword;
-        
-        /// <summary>
         /// btnLogin control.
         /// </summary>
         /// <remarks>
@@ -137,6 +119,15 @@ namespace A2ZGymSolution {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnNewUser;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
