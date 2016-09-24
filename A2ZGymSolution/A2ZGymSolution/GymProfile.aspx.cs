b633a5e48c20fba8752f5296e48ca28efe6922cc
choudharysoni1991 <choudharysoni1991@gmail.com>
2016-09-24 18:13:13 +0530

    intigrated home page rady

--- A2ZGymSolution/A2ZGymSolution/GymProfile.aspx.cs
+++ A2ZGymSolution/A2ZGymSolution/GymProfile.aspx.cs
@@ -12,7 +12,7 @@ namespace A2ZGymSolution
 {
     public partial class GymProfile1 : System.Web.UI.Page
     {
-        public string gymname ="";
+        public string gymname = "";
         public string gymlogo = "";
         public string gymcover = "";
 
@@ -22,19 +22,19 @@ namespace A2ZGymSolution
         DataTable record = new DataTable();
         protected void Page_Load(object sender, EventArgs e)
         {
-      
-           string name = this.Page.RouteData.Values["name"].ToString();
 
-            gymname = name.Replace("-", " ");
+            //string name = this.Page.RouteData.Values["name"].ToString();
 
-            string getPicPath = "select * from gymregistration where gymname ='" + gymname + "'";
-            da = new SqlDataAdapter(getPicPath, connection);
-            da.Fill(record);
-            if (record.Rows.Count > 0)
-            {
-                gymlogo = record.Rows[0]["companylogo"].ToString();
-                gymcover = record.Rows[0]["coverimage"].ToString();
-            }
+            //gymname = name.Replace("-", " ");
+
+            //string getPicPath = "select * from gymregistration where gymname ='" + gymname + "'";
+            //da = new SqlDataAdapter(getPicPath, connection);
+            //da.Fill(record);
+            //if (record.Rows.Count > 0)
+            //{
+            //    gymlogo = record.Rows[0]["companylogo"].ToString();
+            //    gymcover = record.Rows[0]["coverimage"].ToString();
+            //}
         }
     }
  }
